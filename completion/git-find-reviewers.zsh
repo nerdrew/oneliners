1b35f6c343b4154bdad9d5f01d025c346471cd86
Andrew Lazarus <lazarus@squareup.com>
2014-12-08 09:51:32 -0800

    Completions

---
+++ completion/git-find-reviewers.zsh
@@ -0,0 +1,19 @@
+#compdef git-find-reviewers
+
+typeset -A opt_args
+local context state line
+
+_arguments -s -S \
+  "-v[Print line count by file]" \
+  "--verbose[Print line count by file]" \
+  "--no-verbose[Print line count by file]" \
+  "-l[Include git log output]" \
+  "--log[Include git log output]" \
+  "--no-log[Include git log output]" \
+  "-c[Colorize output (default: true)]" \
+  "--color[Colorize output (default: true)]" \
+  "--no-color[Colorize output (default: true)]" \
+  "-d[Print debug information]" \
+  "--debug[Print debug information]" \
+  "--no-debug[Print debug information]" \
+  '*:file:_files' && return 0
