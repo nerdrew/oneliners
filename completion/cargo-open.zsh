542cca5249c1a856a2fcc0b0de04a94ef6bdec42
Andrew Lazarus <lazarus@squareup.com>
2017-06-10 18:24:23 -0700

    updates
    - shellcheck
    - update git-meta-diff to diff logs not diffs
    - consolidate / cleanup completions

--- completion/cargo-open.zsh
+++ completion/cargo-open.zsh
@@ -1,17 +1,22 @@
 #compdef cargo-open -p cargo
 
 _cargo_open() {
-  curr=$words[(( $CURRENT - 1 ))]
-  if [[ $curr == "open" || $curr == "cargo-open" ]]; then
-    _arguments '*: :__crates'
+  if [[ ${words[1]} == "cargo" ]]; then
+    curr=${words[2]}
+    last=2
+  else
+    curr=${words[1]#cargo-}
+    last=1
+  fi
+  if [[ $curr == "open" ]]; then
+    _arguments "$last: :__crates"
     _compskip=all
   fi
 }
 
 __crates() {
   local -a crates;
-  crates=(${(u)$(cargo metadata | jq -r ".packages[].name")})
-
+  crates=($(cargo metadata --format-version=1 | jq -r ".packages[].name" | sort -u))
   _describe 'command' crates
 }
 
---
+++ completion/git-oneliners.zsh
@@ -0,0 +1,43 @@
+#compdef git-fresh git-find-reviewers git-meta-diff git-purge-merged-branches -p git
+
+_git_oneliners() {
+  if [[ ${words[1]} == "git" ]]; then
+    curr=${words[2]}
+    last=2
+  else
+    curr=${words[1]#git-}
+    last=1
+  fi
+  if [[ $curr == "fresh" ]]; then
+    _arguments \
+      "$last: :__git_oneliners_branches"
+    _compskip=all
+  elif [[ $curr == "meta-diff" ]]; then
+    _arguments \
+      "$last: :__git_oneliners_branches" \
+      "$(($last + 1)): :__git_oneliners_branches"
+    _compskip=all
+  elif [[ $curr == "purge-merged-branches" ]]; then
+    _arguments \
+      '--remote[purge merged remote branches]' \
+      '--tags[purge remote tags]' \
+      "$last: :__git_oneliners_branches"
+    _compskip=all
+  elif [[ $curr == "find-reviewers" ]]; then
+    _arguments -s -S \
+      '(-v --verbose)'{-v,--verbose}'[Print line count by file]' \
+      '(-l --log)'{-l,--log}'[Include git log output]' \
+      '(-c --color)'{-c,--color}'[Colorize output (default: true)]' \
+      '(-d --debug)'{-d,--debug}'[Print debug information]' \
+      "$last: :__git_oneliners_branches"
+    _compskip=all
+  fi
+}
+
+__git_oneliners_branches() {
+  local -a branches;
+  branches=(HEAD $(git for-each-ref --format="%(refname:short)" "refs/heads/" "refs/remotes/"))
+  _describe 'command' branches
+}
+
+_git_oneliners
